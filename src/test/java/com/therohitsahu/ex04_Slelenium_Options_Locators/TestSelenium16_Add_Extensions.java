0af820faf844cc67cd8f2722282a28941345dcec
Rohit823823 <sahurohitkumar2001@gmail.com>
2024-12-26 13:10:14 +0530

    All 26th December 2024

---
+++ src/test/java/com/therohitsahu/ex04_Slelenium_Options_Locators/TestSelenium16_Add_Extensions.java
@@ -0,0 +1,56 @@
+package com.therohitsahu.ex04_Slelenium_Options_Locators;
+import io.qameta.allure.Description;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+import java.io.File;
+
+public class TestSelenium16_Add_Extensions {
+    @Description("Options Class")
+    @Test
+    public void test_Selenium01() throws Exception {
+
+        // EdgeOptions, ChromeOptions, FirefoxOptions, SafariOptions
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--window-size=1920,1080");
+        edgeOptions.addExtensions(new File("src/test/java/com/therohitsahu/ex04_Slelenium_Options_Locators/AdBlocker.crx"));
+
+
+//        Proxy proxy = new Proxy();
+//        proxy.setHttpProxy("188.255.245.205:1080");
+//        edgeOptions.setCapability("proxy", proxy);
+
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://www.youtube.com/watch?v=BwTSxsAqlEc");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Thread.sleep(35000);
+        driver.quit();
+        // It will close all the tabs. - session id == null
+
+
+
+
+
+
+
+
+
+
+    }
+}
