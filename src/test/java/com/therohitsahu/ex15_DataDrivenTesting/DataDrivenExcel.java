29dff6367847f9bf53776a44c1a8c709062f957a
Rohit823823 <sahurohitkumar2001@gmail.com>
2025-01-10 07:35:03 +0530

    All practice on 10 January 2025

--- src/test/java/com/therohitsahu/ex15_DataDrivenTesting/DataDrivenExcel.java
+++ src/test/java/com/therohitsahu/ex15_DataDrivenTesting/DataDrivenExcel.java
@@ -9,5 +9,10 @@ public class DataDrivenExcel {
     public void test_VWOLogin (String email,String password) {
         System.out.println("Email - " +email);
         System.out.println("Password - " +password);
+
+        // If email and pass ->  correct ->
+
+        /// Else
+
     }
 }
